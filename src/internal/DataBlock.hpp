857bb882ee28e73fd9f7d00c9169eb7587183696
Luca <luca.courte@hotmail.de>
2024-11-11 23:07:04 +0100

    :bug: Make Datablock more robust

--- src/internal/DataBlock.hpp
+++ src/internal/DataBlock.hpp
@@ -39,9 +39,22 @@ namespace tinyvtu::internal {
      */
     Data compressData(const std::uint8_t *source, std::uint64_t size, const compression::Info &compression);
 
+    /**
+     * Creates a DataBlock object from provided data, name, number of components, and compression info.
+     *
+     * @param name                  The name of the data block
+     * @param data                  The data to be stored in the data block
+     * @param number_of_components  The number of components per data entry (can be zero for internal data)
+     * @param compression           The compression information
+     * @return                      A DataBlock object containing the specified data
+     * @throws std::invalid_argument if the data size is not divisible by the number of components
+     */
     template<typename T> requires NumericType<T>
     DataBlock createBlock(const std::string &name, const std::vector<T> &data, std::uint32_t number_of_components,
                           const compression::Info &compression) {
+        if (number_of_components != 0 and (data.size() % number_of_components != 0)) {
+            throw std::invalid_argument("data size must be divisible by number_of_components");
+        }
         const auto compressedData = compressData(reinterpret_cast<const std::uint8_t *>(data.data()),
                                                  data.size() * sizeof(T), compression);
         if constexpr (sizeof(T) == sizeof(std::uint8_t) && std::is_unsigned_v<T>)
@@ -65,7 +78,7 @@ namespace tinyvtu::internal {
         else if constexpr (sizeof(T) == sizeof(double))
             return {DataBlock::Float64, name, number_of_components, compressedData};
         else
-            static_assert(std::is_integral_v<T>, "Unsupported data type");
+            static_assert(!sizeof(T), "Unsupported data type");
         return {};
     }
 }
